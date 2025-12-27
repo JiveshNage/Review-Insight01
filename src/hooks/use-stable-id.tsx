d1701ff1397c98c1388e58fbadfb6e610c0a6521
jivesh king <kingjiva99@gmail.com>
2025-12-27 17:07:43 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/hooks/use-stable-id.tsx
+++ src/hooks/use-stable-id.tsx
@@ -3,28 +3,24 @@
 
 import * as React from "react"
 
-let count = 0
-function genId() {
-  return (count++).toString()
-}
+// This is a simplified version of the use-id package (https://github.com/radix-ui/primitives/tree/main/packages/react/id)
+// We need to use a client-safe `useId` to ensure that the id is stable during the component's lifecycle.
+// `React.useId()` is not stable between server and client renders.
 
-// We need to use `useMemo` to ensure that the id is stable during the component's lifecycle
+let count = 0;
+const useId =
+  (React as any)['useId'.toString()] ||
+  function useId() {
+    const [id, setId] = React.useState<string | undefined>();
+    React.useEffect(() => {
+      if (!id) {
+        setId('radix-' + count++);
+      }
+    }, [id]);
+    return id;
+  };
+
 export function useStableId(providedId?: string) {
-  return React.useMemo(() => {
-    if (providedId) {
-      return providedId
-    }
-    // Why not use `React.useId()`?
-    //
-    // `React.useId()` is not stable between server and client.
-    //
-    // In our case, the rendered html needs to be the same to avoid hydration errors.
-    //
-    // `React.useId()` is not a good solution for this problem, as it's not designed to be stable across server and client renders.
-    //
-    // This is a simple and effective solution for our use case.
-    return genId()
-  }, [providedId])
+  const id = useId();
+  return providedId || id;
 }
-
-    
